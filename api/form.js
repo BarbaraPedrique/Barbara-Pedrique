98d0b1c6db9fcad239d4d9a40a1469f4504fc7a6
BarbaraPedrique <barbara.pedrique@gmail.com>
2024-06-01 20:28:49 -0400

    tarea lista

--- api/form.js
+++ api/form.js
@@ -9,42 +9,21 @@ router.post("/", async function (req, res, next) {
   try {
     let ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
     ip = ip.split(",")[0];
+    const ipSecret = process.env.IP_LOCALIZATOR_API_KEY;
     const ipResult = await axios.get(
-      "https://ipgeolocation.abstractapi.com/v1/?api_key=c8bb988448b5494dae8291f18ab68446&ip_address=" +
+      "https://ipgeolocation.abstractapi.com/v1/?api_key=" +
+        ipSecret +
+        "&ip_address=" +
         ip
     );
-    const form = await Forms.create({
+    await Forms.create({
       ...req.body,
       ip,
       geolocalization: ipResult.data,
     });
-
-    let transporter = nodemailer.createTransport({
-      host: "smtp.hostinger.com",
-      port: 465, // Use port 465 for SSL
-      secure: true, // Set to true for SSL
-      auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS,
-      },
-    });
-
-    const sendTemplate = {
-      from: "test009@arodu.dev", //correo de ejemplo
-      to: "barbara.pedrique@gmail.com",
-      subject: "Nuevo lead en web",
-      text: `Nombre: ${req.body.name} | Apellidos: ${
-        req.body.lastname
-      } | Email: ${req.body.email} | Date: ${new Date()}`,
-    };
-
-    transporter.sendMail(sendTemplate, (error, info) => {
-      if (error) {
-        console.error("Error sending email:", error);
-      } else {
-        console.log("Email sent successfully:", info.response);
-      }
-    });
+    const localizationString = `${ipResult.data.city}, ${ipResult.data.region}, ${ipResult.data.country}`;
+    sendMail(req, req.body.email, localizationString);
+    sendMail(req, "programacion2ais@dispostable.com", localizationString);
 
     res.render("success");
   } catch (error) {
@@ -53,4 +32,31 @@ router.post("/", async function (req, res, next) {
   }
 });
 
+function sendMail(req, email, gerolocalization) {
+  let transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: 465, // Use port 465 for SSL
+    secure: true, // Set to true for SSL
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
+    },
+  });
+
+  const sendTemplate = {
+    from: "test009@arodu.dev", //correo de ejemplo
+    to: email,
+    subject: "Nuevo lead en web",
+    html: `Nombre: ${req.body.name} | Apellidos: ${req.body.lastname} | Email: ${req.body.email} | Telefono: ${req.body.phone} | Geolocalizacion: ${gerolocalization}
+     <hr> Mensaje: ${req.body.description} `,
+  };
+
+  transporter.sendMail(sendTemplate, (error, info) => {
+    if (error) {
+      console.error("Error sending email:", error);
+    } else {
+      console.log("Email sent successfully:", info.response);
+    }
+  });
+}
 module.exports = router;
--- routes/contact.js
+++ routes/contact.js
@@ -3,7 +3,8 @@ const router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("contact");
+  const recaptchaToken = process.env.RECAPTCHA_SECRET;
+  res.render("contact", { recaptchaToken });
 });
 
 module.exports = router;
